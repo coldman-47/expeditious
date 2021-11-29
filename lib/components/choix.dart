6b0d6b9ce9d9a113b2022b103e766f5213be4cc1
mamimebousso <94001774+mamimebousso@users.noreply.github.com>
2021-11-29 09:55:18 +0000

    Update choix.dart

--- lib/components/choix.dart
+++ lib/components/choix.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 
 class Choix extends StatefulWidget {
@@ -13,74 +12,87 @@ class Choix extends StatefulWidget {
 }
 
 class _ChoixState extends State<Choix> {
-  get child => null;
-
+ 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Container( 
+      padding: const EdgeInsets.all(20),
+      child: Column(mainAxisAlignment: MainAxisAlignment.start,
         children:[
-              Expanded(
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Ink.image(
-                                  image: const AssetImage('Appel.png'),
-                                  height: 100 ,
-                                  width: 100 ,
-                                  fit: BoxFit.cover,),
-                      const SizedBox(height:6),
-                      const Spacer(),
-                      const Text('Recevoir un appel', style: TextStyle(fontSize: 28,) ,)
-                     ] )
-                               ),
-                              
-                      ),
-    
-              Expanded(
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Ink.image(
-                                  image: const AssetImage('vocal.png'),
-                                  height: 100 ,
-                                  width: 100 ,
-                                  fit: BoxFit.cover,),
-                      const SizedBox(height:6),
-                      const Spacer(),
-                      const Text('Faire un vocal', style: TextStyle(fontSize: 28,) ,)
-                     ] )
-                               ),
-                            
-                      ), 
-               Expanded(
-                child: InkWell(
-                  splashColor: Colors.black26,
-                  onTap: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Ink.image(
-                                  image: const AssetImage('formulaire.png'),
-                                  height: 100 ,
-                                  width: 100 ,
-                                  fit: BoxFit.cover,),
-                      const SizedBox(height:6),
-                      const Spacer(),
-                      const Text('Remplir le formulaire', style: TextStyle(fontSize: 28,) ,)
-                     ] )
-                               ),
-                            
-     
-     ) ] 
-    );
+        Row(
+        children:[
+         Material(
+               color: Colors.green,
+               elevation: 8,
+               shape: const CircleBorder(),
+               clipBehavior: Clip.antiAliasWithSaveLayer,
+               child:  InkWell(
+                 splashColor: Colors.greenAccent,
+                 onTap: (){} ,
+                 child: Ink.image(
+                             image: const AssetImage('appel.png'),
+                              width: 100 ,
+                              height: 100 ,
+                              fit: BoxFit.cover,),
+                              )
+                       ),
+         const Spacer(),
+         const Text('Recevoir un appel',   
+                    style: TextStyle(
+                    height: 18,  
+                    fontSize: 20,)
+        )]),
+             
+
+        Row(
+        children:[
+         Material(
+               color: Colors.green,
+               elevation: 8,
+               shape: const CircleBorder(),
+               clipBehavior: Clip.antiAliasWithSaveLayer,
+               child:  InkWell(
+                 splashColor: Colors.greenAccent,
+                 onTap: (){} ,
+                 child: Ink.image(
+                             image: const AssetImage('vocal.png'),
+                              width: 100 ,
+                              height: 100 ,
+                              fit: BoxFit.cover,),
+                              )
+                       ),
+         const Spacer(),
+         const Text('Faire un vocal',   
+                    style: TextStyle(
+                    height: 18,  
+                    fontSize: 20,))]),
        
-                     
+        Row(
+        children:[
+         Material(
+               color: Colors.blue,
+               elevation: 8,
+               shape: const CircleBorder(),
+               clipBehavior: Clip.antiAliasWithSaveLayer,
+               child:  InkWell(
+                 splashColor: Colors.black12,
+                 onTap: (){} ,
+                 child: Ink.image(
+                             image: const AssetImage('formulaire.png'),
+                              width: 100 ,
+                              height: 100 ,
+                              fit: BoxFit.cover,),
+                              )
+                       ),
+         const Spacer(),
+         const Text('Remplir le formulaire',   
+                    style: TextStyle(
+                    height: 18,  
+                    fontSize: 20,
+                    ))]),
+                   
+        ]));
+  
   }
 }
 
