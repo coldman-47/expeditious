f6d1f2858515aef07a076e15184904f9a95782ca
mamimebousso <94001774+mamimebousso@users.noreply.github.com>
2021-12-10 15:30:05 +0000

    Add files via upload

---
+++ lib/api/ApiLivraison.dart
@@ -0,0 +1,27 @@
+
+import 'package:http/http.dart' as http;
+
+import '../model/livraisonModel.dart';
+
+// ignore: camel_case_types
+class APiLivraison {
+  Future<LivraisonModel> getLivraisonData() async {
+    var uri = Uri.parse('https://5b7c-41-83-49-211.ngrok.io/api-doc/#/Livraisons/get_api_livraisons_');
+    var livraisonModel;
+
+     try{
+          final response = await http.get(uri);
+          if(response.statusCode == 200){
+             final String responseString = response.body;
+
+             final livraisonModel = LivraisonModel.fromJson(responseString);
+
+          return livraisonModel;
+  
+    }
+  } catch (e) {
+     
+    }
+    return livraisonModel;
+  }
+}
